f00e1f93e67dfbfdaa99ebbe8e842483ebc78089
Audrey Schmidt <audsch@outlook.com>
2022-03-10 15:12:57 -0600

    docs: adjusted all double-quotes to single-quotes for uniformity

--- app.py
+++ app.py
@@ -8,21 +8,21 @@ def get_recipe(recipe_name):
     json_file = open('data.json', 'r')
     recipes = json.load(json_file)
 
-    for i in range(len(recipes["recipes"])):
-        if recipes["recipes"][i]["name"] == recipe_name :
-            return {'recipe': recipes["recipes"][i], 'index': i } #will end loop once recipe is found
+    for i in range(len(recipes['recipes'])):
+        if recipes['recipes'][i]['name'] == recipe_name :
+            return {'recipe': recipes['recipes'][i], 'index': i } #will end loop once recipe is found
     
     json_file.close()
     return 'None'
     
-@app.route("/recipes", methods=['GET'])
+@app.route('/recipes', methods=['GET'])
 def get_all_recipes():
     json_file = open('data.json', 'r')
     recipes = json.load(json_file)
     json_file.close()
     return make_response(recipes, 200)
 
-@app.route("/recipes/<recipe_name>", methods=['GET'])
+@app.route('/recipes/<recipe_name>', methods=['GET'])
 def get_recipe_by_name(recipe_name):
     selected_recipe = get_recipe(recipe_name)
     if selected_recipe != 'None':
@@ -30,7 +30,7 @@ def get_recipe_by_name(recipe_name):
     else:
         return make_response(jsonify({'error': 'Recipe does not exist'}), 200)
     
-@app.route("/recipes", methods=['POST'])
+@app.route('/recipes', methods=['POST'])
 def add_recipe():
     new_recipe = json.loads(request.data)
     can_create = True
@@ -38,21 +38,21 @@ def add_recipe():
     json_file = open('data.json', 'r')
     recipes = json.load(json_file)
     
-    for recipe in recipes["recipes"]:
+    for recipe in recipes['recipes']:
         if recipe['name'] == new_recipe['name']:
             can_create = False
             break #ends loop if recipe is found
     
     if can_create == True:
         json_file = open('data.json', 'w')
-        recipes["recipes"].append(new_recipe)
+        recipes['recipes'].append(new_recipe)
         json_file.write(json.dumps(recipes, indent=4)) #indent for formatting
         json_file.close()
         return(recipes, 201)
     else:
         return(jsonify({'error': 'Recipe already exists'}), 400)
 
-@app.route("/recipes/<recipe_name>", methods=['PUT'])
+@app.route('/recipes/<recipe_name>', methods=['PUT'])
 def update_recipe(recipe_name):
     selected_recipe = get_recipe(recipe_name)
     json_file = open('data.json', 'r')
@@ -61,14 +61,14 @@ def update_recipe(recipe_name):
     if selected_recipe != 'None':
         updated_recipe = json.loads(request.data)
         json_file = open('data.json', 'w')
-        recipes["recipes"][selected_recipe["index"]] = updated_recipe
+        recipes['recipes'][selected_recipe['index']] = updated_recipe
         json_file.write(json.dumps(recipes, indent=4))
         json_file.close()
         return make_response(jsonify({'details': updated_recipe}), 204)
     else:
         return make_response(jsonify({'error': 'Recipe does not exist'}), 404)
 
-@app.route("/recipes/<recipe_name>", methods=['DELETE'])
+@app.route('/recipes/<recipe_name>', methods=['DELETE'])
 def delete_recipe(recipe_name):
     selected_recipe = get_recipe(recipe_name)
     json_file = open('data.json', 'r')
@@ -76,7 +76,7 @@ def delete_recipe(recipe_name):
     
     if selected_recipe != 'None':
         json_file = open('data.json', 'w')
-        recipes["recipes"].pop(selected_recipe['index'])
+        recipes['recipes'].pop(selected_recipe['index'])
         json_file.write(json.dumps(recipes, indent=4))
         json_file.close()
         return make_response({}, 204) #ends loop if recipe is found
